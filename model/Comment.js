e85e211f9b6e876b121b592e80208ed1d998a427
Tracey Kadenyi <treykadenyi@gmail.com>
2023-11-14 22:58:56 +0300

    Edit relationship with replies

--- model/Comment.js
+++ model/Comment.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Story = require("./Story");
+const Reply = require("./Reply");
 
 const CommentSchema = new Schema({
   commenter: {
@@ -25,9 +26,12 @@ const CommentSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "Story",
   },
-  reply: {
-    type: [this],
-  },
+  replies: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Reply",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
