f7bc5f798230a6a21938af047ce33b70119baa23
Tracey Kadenyi <treykadenyi@gmail.com>
2023-11-14 21:58:30 +0300

    Add new relationship

--- model/Comment.js
+++ model/Comment.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Story = require("./Story");
 
 const CommentSchema = new Schema({
   commenter: {
@@ -19,6 +20,10 @@ const CommentSchema = new Schema({
     type: String,
     default: new Date().toLocaleTimeString(),
     required: true,
+  },
+  story: {
+    type: Schema.Types.ObjectId,
+    ref: "Story",
   },
   reply: {
     type: [this],
