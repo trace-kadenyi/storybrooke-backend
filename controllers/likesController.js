9f85c8c5c4f0364ba675ebbbed35cb9848280305
Tracey Kadenyi <treykadenyi@gmail.com>
2024-03-06 15:47:01 +0300

    Edit and create functions to update and get likes

--- controllers/likesController.js
+++ controllers/likesController.js
@@ -1,23 +1,72 @@
 const Comment = require("../model/Comment");
 const Reply = require("../model/Reply");
+const User = require("../model/User");
 
 // update comment likes
+// const updateCommentLikes = async (req, res) => {
+//   // check if comment id is valid
+//   const comment = await Comment.findOne({ _id: req.params.id });
+
+//   if (!comment) {
+//     res.status(404).json({ message: "Comment not found" });
+//     return;
+//   }
+
+//   // update comment likes
+//   try {
+//     comment.likes = req.body.likes;
+//     await comment.save();
+//     res.status(200).json({ message: "Comment likes updated" });
+//   } catch (err) {
+//     res.status(400).json({ message: err.message });
+//   }
+// };
+
 const updateCommentLikes = async (req, res) => {
-  // check if comment id is valid
-  const comment = await Comment.findOne({ _id: req.params.id });
-
-  if (!comment) {
-    res.status(404).json({ message: "Comment not found" });
-    return;
-  }
-
-  // update comment likes
   try {
-    comment.likes = req.body.likes;
+    // check if comment id is valid
+    const comment = await Comment.findOne({ _id: req.params.id });
+
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    // get username from body
+    const username = req.body.username;
+
+    // Check if the user has already liked the comment
+    if (comment.likes.includes(username)) {
+      // remove username
+      comment.likes = comment.likes.filter((like) => like !== username);
+      await comment.save();
+      return res.status(200).json({ message: "Comment unliked successfully" });
+    }
+
+    // Update comment likes
+    comment.likes.push(username);
     await comment.save();
-    res.status(200).json({ message: "Comment likes updated" });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
+
+    return res.status(200).json({ message: "Comment liked successfully" });
+  } catch (error) {
+    console.error("Error updating comment likes:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// get comment likes
+const getCommentLikes = async (req, res) => {
+  try {
+    // check if comment id is valid
+    const comment = await Comment.findOne({ _id: req.params.id });
+
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    return res.status(200).json({ likes: comment.likes.length });
+  } catch (error) {
+    console.error("Error getting comment likes:", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -44,4 +93,5 @@ const updateReplyLikes = async (req, res) => {
 module.exports = {
   updateCommentLikes,
   updateReplyLikes,
+  getCommentLikes,
 };
