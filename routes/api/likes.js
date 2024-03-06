44edfd0d8f876ab68216ef8d9cb5335407d3efcc
Tracey Kadenyi <treykadenyi@gmail.com>
2024-03-06 15:47:12 +0300

    Add route for getting likes

--- routes/api/likes.js
+++ routes/api/likes.js
@@ -4,5 +4,6 @@ const likesController = require("../../controllers/likesController");
 
 router.route("/comments/:id").put(likesController.updateCommentLikes);
 router.route("/replies/:id").put(likesController.updateReplyLikes);
+router.route("/comments/:id").get(likesController.getCommentLikes);
 
 module.exports = router;
