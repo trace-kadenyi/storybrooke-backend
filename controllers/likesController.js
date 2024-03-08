f6fbc85cca5ccf74eec9c9d98e7eec24d3a44d88
Tracey Kadenyi <treykadenyi@gmail.com>
2024-03-08 23:59:14 +0300

    Implement functionalities for reply likes

--- controllers/likesController.js
+++ controllers/likesController.js
@@ -53,21 +53,50 @@ const getCommentLikes = async (req, res) => {
 
 // update reply likes
 const updateReplyLikes = async (req, res) => {
-  // check if reply id is valid
-  const reply = await Reply.findOne({ _id: req.params.id });
-
-  if (!reply) {
-    res.status(404).json({ message: "Reply not found" });
-    return;
-  }
-
-  // update reply likes
   try {
-    reply.likes = req.body.likes;
+    // check if reply id is valid
+    const reply = await Reply.findOne({ _id: req.params.id });
+
+    // if (!reply) {
+    //   return res.status(404).json({ message: "reply not found" });
+    // }
+
+    // get username from body
+    const username = req.body.username;
+
+    // Check if the user has already liked the comment
+    if (reply.likes.includes(username)) {
+      // remove username
+      reply.likes = reply.likes.filter((like) => like !== username);
+      await reply.save();
+      return res.status(200).json({ message: "Reply unliked successfully" });
+    }
+
+    // Update comment likes
+    reply.likes.push(username);
     await reply.save();
-    res.status(200).json({ message: "Reply likes updated" });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
+
+    return res.status(200).json({ message: "Reply liked successfully" });
+  } catch (error) {
+    console.error("Error updating reply likes:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// get reply likes
+const getReplyLikes = async (req, res) => {
+  try {
+    // check if reply id is valid
+    const reply = await Reply.findOne({ _id: req.params.id });
+
+    if (!reply) {
+      return res.status(404).json({ message: "Reply not found" });
+    }
+
+    return res.status(200).json({ likes: reply.likes.length });
+  } catch (error) {
+    console.error("Error getting reply likes:", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -75,4 +104,5 @@ module.exports = {
   updateCommentLikes,
   updateReplyLikes,
   getCommentLikes,
+  getReplyLikes,
 };
