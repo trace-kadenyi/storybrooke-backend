21268071b6f041278466ceda194a883eb1f91de7
Tracey Kadenyi <treykadenyi@gmail.com>
2024-03-08 23:51:57 +0300

    Add route for reply likes

--- routes/api/likes.js
+++ routes/api/likes.js
@@ -5,5 +5,6 @@ const likesController = require("../../controllers/likesController");
 router.route("/comments/:id").put(likesController.updateCommentLikes);
 router.route("/replies/:id").put(likesController.updateReplyLikes);
 router.route("/comments/:id").get(likesController.getCommentLikes);
+router.route("/replies/:id").get(likesController.getReplyLikes);
 
 module.exports = router;
