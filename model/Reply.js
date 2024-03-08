84ea738250eddab8e4527efd7fb229bef7139cdb
Tracey Kadenyi <treykadenyi@gmail.com>
2024-03-08 23:52:09 +0300

    Modify likes in model

--- model/Reply.js
+++ model/Reply.js
@@ -3,33 +3,29 @@ const Schema = mongoose.Schema;
 const Comment = require("./Comment");
 
 const ReplySchema = new Schema({
-    commenter: {
-        type: String,
-        required: true,
-    },
-    body: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: String,
-        default: new Date(),
-        required: true,
-    },
-    time: {
-        type: String,
-        default: new Date().toLocaleTimeString(),
-        required: true,
-    },
-    likes: {
-        type: Number,
-        default: 0,
-        required: true,
-    },
-    comment: {
-        type: Schema.Types.ObjectId,
-        ref: "Comment",
-    },
-    });
+  commenter: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    default: new Date(),
+    required: true,
+  },
+  time: {
+    type: String,
+    default: new Date().toLocaleTimeString(),
+    required: true,
+  },
+  likes: [String],
+  comment: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment",
+  },
+});
 
 module.exports = mongoose.model("Reply", ReplySchema);
