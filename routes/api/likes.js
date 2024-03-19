3c1efea15f0db53cbe18cb185d75b61e5fda875e
Tracey Kadenyi <treykadenyi@gmail.com>
2024-03-19 22:38:44 +0300

    Modify routes

--- routes/api/likes.js
+++ routes/api/likes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const likesController = require("../../controllers/likesController");
 
-router.route("/comments/:id").put(likesController.updateCommentLikes);
-router.route("/replies/:id").put(likesController.updateReplyLikes);
-router.route("/comments/:id").get(likesController.getCommentLikes);
-router.route("/replies/:id").get(likesController.getReplyLikes);
+router.route("/comment/:id").put(likesController.updateCommentLikes);
+router.route("/reply/:id").put(likesController.updateReplyLikes);
+router.route("/comment/:id").get(likesController.getCommentLikes);
+router.route("/reply/:id").get(likesController.getReplyLikes);
+router.route("/story/:id").put(likesController.updateStoryLikes);
+router.route("/story/:id").get(likesController.getStoryLikes);
 
 module.exports = router;
